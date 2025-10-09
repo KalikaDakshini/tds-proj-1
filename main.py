27d07349c047778e2135dc34b28ead6bb820b191
Kalika <kalikadakshini@gmail.com>
2025-10-09 14:23:08 +0530

    Added a license and CORS

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 """Main file of the application."""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import router
 
 
 app = FastAPI()
 app.include_router(router)
+
+# Add CORS. Allow all origins (for testing / public API)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
 
 @app.get("/")
